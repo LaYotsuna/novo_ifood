98875b5868cc2991ebef5103592b8c10d3171cc4
Larissa Cerqueira <109742068+LaYotsuna@users.noreply.github.com>
2023-10-27 20:59:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 "use strict";
-/* Não finalizado */
-document.getElementById("icone-menu").addEventListener("click", function () {
-  let menu = document.getElementById("menu-mobile");
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-  } else {
-    menu.classList.add("active");
-  }
-});
-
 // Quando o cursor estiver na navbar a notficação aparecerá
 function notificacaoCaraNova() {
   const navbar = document.querySelector(".navbar");
